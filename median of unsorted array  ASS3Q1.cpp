4ced7840839f6962c2125e6a466e1222f3b9c16f
barvin04 <abhinababala04@gmail.com>
2017-08-29 05:29:36 +0530

    finding median in an unsorted array without actually sorting them !

---
+++ median of unsorted array  ASS3Q1.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+//FOR THE TIME BEING ASSUME THE ARRAY TO BE ODD 
+
+int main(){
+int N;
+cout<<"enter the number of elements in the array"<<endl;
+cin>>N;
+int arr[N], b[N];
+
+cout<<"enter the integers : "<< endl;
+for (int i=0; i<N; i++){cin>>arr[i];}
+
+
+//pick up a reference element, ... let it be arr[0]
+anchor = arr[0]
+
+
+//now compare and put 'lows' to left and 'highs' to right 
+test = true;
+int l=0, r=N-1;
+int n =N;
+while(test){
+for (int i=l; i<r; i++){
+	if (arr[i]<= anchor){b[l] = arr[i]; l++;}
+	if (arr[i]>anchor){b[r] = arr[i]; r--;}
+	}
+arr = b;
+n=r;
+if (l+1 == N/2){cout<<b[N/2]<<endl; test=false;break;}
+if (r>n/2){
+	l=0;
+	n=r;
+	}
+
+
+if (r<n/2){
+	l = l;
+	r = n;}
+
+
+
+}
+
+}
+
